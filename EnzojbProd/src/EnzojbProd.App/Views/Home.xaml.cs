02e2866179c32cc0418bdb4fad5d0e4c316f3814
Carlos Alberto <carlosalbertode.campos@gmail.com>
2022-12-31 09:31:46 -0300

    Removing info inventories from home page and sidebar

--- EnzojbProd/src/EnzojbProd.App/Views/Home.xaml.cs
+++ EnzojbProd/src/EnzojbProd.App/Views/Home.xaml.cs
@@ -6,19 +6,13 @@ namespace EnzojbProd.App.Views;
 public partial class Home : ContentPage
 {
 	private readonly ProductsRepository _productsRepository;
-	private readonly InventoryRepository _inventoryRepository;
 
 	internal async Task LoadData()
 	{
 		var products = await _productsRepository.GetAllAsync();
-		var inventory = await _inventoryRepository.GetAllAsync();
 		HomeViewModel homeViewData = new HomeViewModel();
 		homeViewData.RegisteredProducts = products.Count();
 		homeViewData.StockProducts = products.Sum(x => x.Stock);
-		homeViewData.CreatedInventories = inventory.Count(x => x.Status == Enums.InventoryStatus.Created);
-		homeViewData.StartedInventories = inventory.Count(x => x.Status == Enums.InventoryStatus.Started);
-		homeViewData.FinishedInventories = inventory.Count(x => x.Status == Enums.InventoryStatus.Finished);
-		homeViewData.Inventories = inventory.Where(x => x.Status == Enums.InventoryStatus.Started);
 		BindingContext = homeViewData;
 	}
 
@@ -27,7 +21,6 @@ public partial class Home : ContentPage
 	{
 		InitializeComponent();
 		_productsRepository = productsRepository;
-		_inventoryRepository = inventoryRepository;
 	}
 
 	protected override async void OnNavigatedTo(NavigatedToEventArgs args)
